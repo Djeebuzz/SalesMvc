afb2c541d379fa5c491e35f2d21c4a9fa3010e5a
eugenio silva <eugeniomsilva90@gmail.com>
2026-07-29 08:17:55 +0100

    Project Created

---
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using System.Runtime.Intrinsics.X86;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Sales Web MVC App for C# Course.";
+           
+            ViewData["Professor"] = "Nelio Alves";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
